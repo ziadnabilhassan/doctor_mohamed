ac361330bd32c4fa5189a4517d3bf9037863a7ad
ziad nabil <116813956+ziadnabilhassan@users.noreply.github.com>
2025-07-10 14:06:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -124,3 +124,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // pageScroll()
 
+// Activating Lateral Menu
+
+const showMenu = (toggleId, navId) => {
+    const toggle =  document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+            toggle.classList.toggle('bx-x');
+        })
+    }
+}
+
+showMenu('header-toggle', 'nav-menu');
+
+// Changing Active Link
+
+const navLink = document.querySelectorAll('.nav-link');
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
